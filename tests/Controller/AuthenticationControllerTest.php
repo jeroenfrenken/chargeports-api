6bf5042af5c4546784b6f3806fe7f199f43a2da0
ml90864 <martinvanliere@outlook.com>
2020-09-28 20:16:14 +0200

    Basic PHPunit setup

---
+++ tests/Controller/AuthenticationControllerTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Tests\Controller;
+
+use App\Controller\AuthenticationController;
+use PHPUnit\Framework\TestCase;
+
+class AuthenticationControllerTest extends TestCase
+{
+
+}
