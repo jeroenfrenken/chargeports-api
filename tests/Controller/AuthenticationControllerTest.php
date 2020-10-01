484513222965db171cfbffca64aeeccf7168f961
ml90864 <martinvanliere@outlook.com>
2020-10-01 12:47:57 +0200

    set up api test using guzzle and PHPUnit

--- tests/Controller/AuthenticationControllerTest.php
+++ tests/Controller/AuthenticationControllerTest.php
@@ -4,8 +4,41 @@ namespace App\Tests\Controller;
 
 use App\Controller\AuthenticationController;
 use PHPUnit\Framework\TestCase;
+use GuzzleHttp;
 
 class AuthenticationControllerTest extends TestCase
 {
+    private $http;
+
+    public function setUp() : void
+    {
+        $this->http = new GuzzleHttp\Client(['base_uri' => 'https://httpbin.org/']);
+    }
+
+    public function tearDown() : void {
+        $this->http = null;
+    }
+
+    public function testGet()
+    {
+        $response = $this->http->request('GET', 'user-agent');
+
+        $this->assertEquals(200, $response->getStatusCode());
+
+        $contentType = $response->getHeaders()["Content-Type"][0];
+        $this->assertEquals("application/json", $contentType);
+
+        $userAgent = json_decode($response->getBody())->{"user-agent"};
+        $this->assertRegexp('/Guzzle/', $userAgent);
+    }
+
+    public function testPut()
+    {
+        $response = $this->http->request('PUT', 'user-agent', ['http_errors' => false]);
+
+        $this->assertEquals(405, $response->getStatusCode());
+    }
+
+
 
 }
