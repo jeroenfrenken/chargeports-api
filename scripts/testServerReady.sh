7a26969bae62ddafa1bd2c72b6eaa16f04934a2c
ml90864 <martinvanliere@outlook.com>
2020-10-05 20:41:34 +0200

    add informational text to script to test if server is ready.

---
+++ scripts/testServerReady.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+attempt_counter=0
+max_attempts=12
+
+until $(curl --output /dev/null --silent --head --fail http://localhost/api/doc); do
+  if [ ${attempt_counter} -eq ${max_attempts} ];then
+    echo "Max attempts reached"
+    exit 1
+  fi
+
+  echo 'No response yet, retrying in 5 seconds'
+  attempt_counter=$(($attempt_counter+1))
+  sleep 5
+done
+
+#Extra sleep just in case
+sleep 5
+echo 'Server is available, proceeding to next step.'
