69c07424cb5198fa075e071b0a55bca5c801aa9f
Jeroen Frenken <jeroenfrenken@icloud.com>
2020-10-31 20:57:02 +0100

    Fixes & upcoming / previous reservations

--- src/Repository/ReservationRepository.php
+++ src/Repository/ReservationRepository.php
@@ -3,8 +3,10 @@
 namespace App\Repository;
 
 use App\Entity\Reservation;
+use DateTime;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
  * @method Reservation|null find($id, $lockMode = null, $lockVersion = null)
@@ -19,32 +21,39 @@ class ReservationRepository extends ServiceEntityRepository
         parent::__construct($registry, Reservation::class);
     }
 
-    // /**
-    //  * @return Reservation[] Returns an array of Reservation objects
-    //  */
-    /*
-    public function findByExampleField($value)
+    /**
+     * @param DateTime $dateTime
+     * @param UserInterface $user
+     * @return Reservation[] Returns an array of Reservation objects
+     */
+    public function findAfterTime(DateTime $dateTime, UserInterface $user)
     {
         return $this->createQueryBuilder('r')
-            ->andWhere('r.exampleField = :val')
-            ->setParameter('val', $value)
+            ->andWhere('r.startTime > :startTime')
+            ->andWhere('r.user = :user')
+            ->setParameter('startTime', $dateTime)
+            ->setParameter('user', $user)
             ->orderBy('r.id', 'ASC')
-            ->setMaxResults(10)
+            ->setMaxResults(50)
             ->getQuery()
-            ->getResult()
-        ;
+            ->getResult();
     }
-    */
 
-    /*
-    public function findOneBySomeField($value): ?Reservation
+    /**
+     * @param DateTime $dateTime
+     * @param UserInterface $user
+     * @return Reservation[] Returns an array of Reservation objects
+     */
+    public function findBeforeTime(DateTime $dateTime, UserInterface $user)
     {
         return $this->createQueryBuilder('r')
-            ->andWhere('r.exampleField = :val')
-            ->setParameter('val', $value)
+            ->andWhere('r.endTime < :endTime')
+            ->andWhere('r.user = :user')
+            ->setParameter('endTime', $dateTime)
+            ->setParameter('user', $user)
+            ->orderBy('r.id', 'ASC')
+            ->setMaxResults(50)
             ->getQuery()
-            ->getOneOrNullResult()
-        ;
+            ->getResult();
     }
-    */
 }
